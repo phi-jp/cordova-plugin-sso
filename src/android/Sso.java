db59ab733f8b35c6e306f53901ffe64996da22ab
shogo <gooodsun1@gmail.com>
2018-02-10 05:44:50 +0900

    facebook login for android

--- src/android/Sso.java
+++ src/android/Sso.java
@@ -11,17 +11,51 @@ import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 import com.twitter.sdk.android.core.models.*;
 import com.twitter.sdk.android.core.services.*;
 
+import com.facebook.Profile;
+import com.facebook.ProfileManager;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookDialogException;
+import com.facebook.FacebookException;
+import com.facebook.FacebookOperationCanceledException;
+import com.facebook.FacebookRequestError;
+import com.facebook.FacebookSdk;
+import com.facebook.FacebookServiceException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.FacebookAuthorizationException;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.applinks.AppLinkData;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.share.ShareApi;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.GameRequestContent;
+import com.facebook.share.model.ShareHashtag;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.ShareOpenGraphObject;
+import com.facebook.share.model.ShareOpenGraphAction;
+import com.facebook.share.model.ShareOpenGraphContent;
+import com.facebook.share.model.AppInviteContent;
+import com.facebook.share.widget.GameRequestDialog;
+import com.facebook.share.widget.MessageDialog;
+import com.facebook.share.widget.ShareDialog;
+import com.facebook.share.widget.AppInviteDialog;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
 
-import android.util.Log;
 import jdk.nashorn.api.scripting.JSObject;
 import jdk.nashorn.internal.runtime.JSONListAdapter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,48 +65,61 @@ import retrofit2.Call;
 
 import com.google.gson.Gson;
 
-public class Sso extends CordovaPlugin {
+import java.util.Arrays;
 
+public class Sso extends CordovaPlugin {
 	private static final String LOG_TAG = "Twitter Connect";
 	private String action;
 	private CallbackContext callbackContext;
+	private CallbackManager fbCallbackManager;
 
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		TwitterConfig config = new TwitterConfig.Builder(cordova.getActivity().getApplicationContext())
     		.twitterAuthConfig(new TwitterAuthConfig(getTwitterKey(), getTwitterSecret()))
     		.build();
 		Twitter.initialize(config);
-
-
 	}
 
-	private String getTwitterKey() {
-		return preferences.getString("TwitterConsumerKey", "");
-	}
-
-	private String getTwitterSecret() {
-		return preferences.getString("TwitterConsumerSecret", "");
-	}
-
-	private String getLineChannelId() {
-		return preferences.getString("LineChannelId", "");
-	}
 	public boolean execute(final String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 		this.action = action;
 		final Activity activity = this.cordova.getActivity();
 		final Context context = activity.getApplicationContext();
-		cordova.setActivityResultCallback(this);
+		this.callbackContext = callbackContext;
 
 		if (action.equals("loginWithTwitter")) {
+			cordova.setActivityResultCallback(this);
 			loginWithTwitter(activity, callbackContext);
 			return true;
 		}
 		else if (action.equals("loginWithLine")) {
-			this.callbackContext = callbackContext;
+			Intent loginIntent = LineLoginApi.getLoginIntent(context, getLineChannelId());
 			loginWithLine(activity, callbackContext);
 			return true;
 		}
-		return false;
+		else if (action.equals("loginWithFacebook")) {
+			cordova.setActivityResultCallback(this);
+			loginWithFacebook(activity, callbackContext);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		super.onActivityResult(requestCode, resultCode, intent);
+		if (action.equals("loginWithTwitter")) {
+			handleTwitterLoginResult(requestCode, resultCode, intent);
+		}
+		else if (action.equals("loginWithLine")) {
+			LineLoginResult result = LineLoginApi.getLoginResultFromIntent(intent);
+			callbackContext.success(getLineBaseUserData(result));
+		}
+		else if (action.equals("loginWithFacebook")) {
+			fbCallbackManager.onActivityResult(requestCode, resultCode, intent);
+
+		}
 	}
 
 	private void loginWithTwitter(final Activity activity, final CallbackContext callbackContext) {
@@ -127,21 +174,45 @@ public class Sso extends CordovaPlugin {
 		this.cordova.startActivityForResult((CordovaPlugin) this, loginIntent, 0);
 	}
 
-	private void handleTwitterLoginResult(int requestCode, int resultCode, Intent intent) {
-		TwitterLoginButton twitterLoginButton = new TwitterLoginButton(cordova.getActivity());
-		twitterLoginButton.onActivityResult(requestCode, resultCode, intent);
+	private void loginWithFacebook(final Activity activity, final CallbackContext callbackContext) {
+		LoginManager.getInstance().logInWithReadPermissions(activity, Arrays.asList("public_profile", "email", "user_friends"));
+		cordova.getThreadPool().execute(new Runnable() {
+			@Override
+			public void run() {
+				fbCallbackManager = CallbackManager.Factory.create();
+				LoginManager loginManager = LoginManager.getInstance();
+				loginManager.registerCallback(fbCallbackManager, new FacebookCallback<LoginResult>() {
+					@Override
+					public void onSuccess(LoginResult loginResult) {
+						JSONObject result = getFacebookBaseUserData(loginResult);
+
+						callbackContext.success(result);
+					}
+
+					@Override
+					public void onCancel() {
+
+					}
+
+					@Override
+					public void onError(FacebookException error) {
+
+					}
+				});
+			}
+		});
 	}
 
-	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		super.onActivityResult(requestCode, resultCode, intent);
-		if (action.equals("loginWithTwitter")) {
-			handleTwitterLoginResult(requestCode, resultCode, intent);
-		}
-		else if (action.equals("loginWithLine")) {
-			LineLoginResult result = LineLoginApi.getLoginResultFromIntent(intent);
-			this.callbackContext.success(getLineBaseUserData(result));
-		}
+	private String getTwitterKey() {
+		return preferences.getString("TwitterConsumerKey", "");
+	}
 
+	private String getTwitterSecret() {
+		return preferences.getString("TwitterConsumerSecret", "");
+	}
+
+	private String getLineChannelId() {
+		return preferences.getString("LineChannelId", "");
 	}
 
 
@@ -189,7 +260,25 @@ public class Sso extends CordovaPlugin {
 		return jsonUser;
 	}
 
+	private JSONObject getFacebookBaseUserData(LoginResult result) {
+		Profile profile = Profile.getCurrentProfile();
+		Uri imageUri = Uri.parse(profile.getProfilePictureUri(320, 320).toString());
 
+		JSONObject jsonUser = new JSONObject();
+		try {
+			jsonUser.put("name", profile.getName() );
+			jsonUser.put("first_name", profile.getFirstName() );
+			jsonUser.put("last_name", profile.getLastName() );
+			jsonUser.put("token", result.getAccessToken().getToken());
+			jsonUser.put("userId", result.getAccessToken().getUserId());
+			jsonUser.put("image", imageUri.toString());
+
+		} catch(JSONException e) {
+			e.printStackTrace();
+		}
+
+		return jsonUser;
+	}
 
 	private JSONObject TweetObjectToJSON(Tweet tweet) {
 		Gson gson = new Gson();
@@ -203,6 +292,11 @@ public class Sso extends CordovaPlugin {
 		}
 
 		return jsonTweet;
+	}
+
+	private void handleTwitterLoginResult(int requestCode, int resultCode, Intent intent) {
+		TwitterLoginButton twitterLoginButton = new TwitterLoginButton(cordova.getActivity());
+		twitterLoginButton.onActivityResult(requestCode, resultCode, intent);
 	}
 
 }
